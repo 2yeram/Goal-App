57b71e7b3b592c8d83349db67bd0baa8d8f4882f
2yeram <ramramm_@naver.com>
2023-12-24 11:35:17 +0900

    event

--- App.js
+++ App.js
@@ -1,14 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
+import { useState } from 'react';
 import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 
 export default function App() {
+  const [enteredGoalText, setEnteredGoalText] = useState('');
+
+  {/* 사용자 입력값 가져오기 */ }
+  function goalInputHandler(enteredText) {
+    setEnteredGoalText(enteredText);
+  };
+
+  {/* 버튼 누르면 작동 */ }
+  function addGoalHandler() {
+    console.log(enteredGoalText);
+  };
+
   return (
     <View style={styles.appContainer}>
-      <View style = {styles.inputContainer}>
+      <View style={styles.inputContainer}>
         {/* 목표 입력 */}
-        <TextInput style={styles.textInput} placeholder='Your course goal!'/>
-        <Button title="Add Goal" />
-        </View>
+        <TextInput style={styles.textInput} placeholder='Your course goal!' onChangeText={goalInputHandler} />
+        <Button title="Add Goal" onPress={addGoalHandler} />
+      </View>
       <View style={styles.goalsContainer}>
         {/* 목표 출력 */}
         <Text>List of goals...</Text>
